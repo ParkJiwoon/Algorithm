96594c5f13047f4184d63f04d29451c2d674e864
bcp0109 <bcp0109@gmail.com>
2018-12-10 01:22:21 +0900

    2839

---
+++ boj/2839.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+// https://www.acmicpc.net/problem/2839
+
+int main() {
+    int kg[5001];
+
+    printf("%d", kg[0]);
+
+    for(int i=0; i<5001; i++) {
+        if(i < 50)
+    }
+}
