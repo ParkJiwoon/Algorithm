f3a772920adfb133e89ce24f9aa27cd4320e6d2a
woody.j <kakao@kakaoui-MacBook-Pro.local>
2019-01-13 20:01:55 +0900

    수 정렬하기

--- boj/2750.java
+++ boj/2750.java
@@ -16,11 +16,23 @@ class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
+        bubbleSort(arr, N);
 
         for(int i=0; i<N; i++) {
             bw.write(String.valueOf(arr[i]) + "\n");
         }
         bw.flush();
+    }
+
+    static void bubbleSort(int[] arr, int N) {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
     }
 }
