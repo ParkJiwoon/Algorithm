d63113c5fa929365437ed15626bb767e0055a366
woody.j <kakao@kakaoui-MacBook-Pro.local>
2019-01-15 10:55:40 +0900

    정상회담2

--- boj/1670.java
+++ boj/1670.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.io.*;
 
 // https://www.acmicpc.net/problem/1670
@@ -10,17 +9,11 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = stoi(br.readLine());
-        long[] dp = new long[n+1];
-        long div = 987654321;
+        long[] dp = new long[100001];
+        final long div = 987654321;
 
         dp[0] = 1;
         dp[2] = 1;
-        // dp[4] += dp[2] * dp[0]
-        // dp[4] += dp[2] * dp[0];
-        
-        // dp[6] += dp[4] * dp[0];
-        // dp[6] += dp[2] * dp[2];
-        // dp[6] += dp[0] * dp[4];
 
         /**
          * 점 하나를 기준으로 다른 점과 하나씩 연결 한 후에 갈라진 점의 경우의 수를 세서 곱한다
