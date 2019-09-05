88184409029d7ac6e54322dcc626af015aa92c73
bcp0109 <bcp0109@gmail.com>
2019-09-06 02:25:32 +0900

    2017 카카오 블라인드 1차 비밀지도

--- Kakao/2017_secret_map.java
+++ Kakao/2017_secret_map.java
@@ -1,33 +1,17 @@
 // https://programmers.co.kr/learn/courses/30/lessons/17681
 
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
+    static public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
         for(int i=0; i<n; i++) {          
             String temp = Integer.toBinaryString(arr1[i] | arr2[i]);  
-            String ans = "";
-            //System.out.println(temp);
-            for(int k=0; k<temp.length(); k++) {
-                if(temp.charAt(k) == '1')
-                    ans += "#";
-                else 
-                    ans += " ";
-            }
-            
-            while(true) {
-                if(ans.length() == n)
-                    break;
-                else {
-                    ans = " " + ans;
-                }
-            }
-            
-            System.out.println(ans);
-            
-            answer[i] = ans;
+            temp = String.format("%" + n + "s", temp);
+            temp = temp.replaceAll("1", "#");
+            temp = temp.replaceAll("0", " ");
+            answer[i] = temp;
         }
         
         return answer;
     }
-  }
+}
