b6c22f8ece4819489a639f74bda3a5acd9a30e39
ParkJiwoon <bcp0109@gmail.com>
2019-09-09 23:46:30 +0900

    Delete 1000.kt

--- boj/1000.kt
+++
@@ -1,7 +0,0 @@
-import java.io.*
-import java.util.*
-
-fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val st = StringTokenizer(readLine())
-    println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
-}
