098531740fa6cafc4b5e9ae12f239ee9225794c4
ParkJiwoon <bcp0109@gmail.com>
2019-09-12 16:47:34 +0900

    15904번. UCPC는 무엇의 약자일까?

---
+++ boj/java/15904.java
@@ -0,0 +1,27 @@
+// https://www.acmicpc.net/problem/15904
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(br.readLine()));
+    }
+
+    static String solution(String s) {
+        char[] ucpc = {'U', 'C', 'P', 'C'};
+        int index = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == ucpc[index]) {
+                index++;
+            }
+
+            if(index == 4) {
+                return "I love UCPC";
+            }
+        }
+
+        return "I hate UCPC";
+    }
+}
