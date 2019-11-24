a60fe241d557f0c1659768c28c47c7cc9fdc08ec
ParkJiwoon <bcp0109@gmail.com>
2019-11-25 01:43:25 +0900

    Update 완주하지 못한 선수.java

--- Programmers/완주하지 못한 선수.java
+++ Programmers/완주하지 못한 선수.java
@@ -1,7 +1,12 @@
 /*
  * https://programmers.co.kr/learn/courses/30/lessons/42576
+ * 
  * 완주하지 못한 선수
  * 
+ * <풀이>
+ * HashMap 에 완주한 선수들의 { 이름: 인원 } 를 저장 후
+ * 참가자들 for 문 돌며 있으면 인원을 하나씩 빼줌
+ * 만약 음수가 된다면 완주하지 못한 선수
  */
 
 import java.util.*;
