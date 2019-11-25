ebe53d8ed27d041ed4e8f6d13a3de7cf3630bc5f
ParkJiwoon <bcp0109@gmail.com>
2019-11-25 23:31:48 +0900

    Leetcode Path Sum III Kotlin

--- leetcode/path-sum-iii.kt
+++ leetcode/path-sum-iii.kt
@@ -1,12 +1,15 @@
 /**
- * first my code
- * https://leetcode.com/problems/path-sum-iii/submissions/
- */
+ * 문제: https://leetcode.com/problems/path-sum-iii
+ * 풀이: https://bcp0109.tistory.com/130
+ */ 
+
+// Using backtracking with List
+// Time complexity: O(n^2)
 class Solution {
     var count = 0
     
     fun pathSum(root: TreeNode?, sum: Int): Int {
-        if (root == null) return 0
+        root ?: return 0
         if (root.`val` == sum) count++
         
         pathChild(root.left, mutableListOf(root.`val`), sum)
@@ -15,7 +18,7 @@ class Solution {
     }
     
     fun pathChild(current: TreeNode?, vals: MutableList<Int>, sum: Int) {
-        if (current == null) return
+        current ?: return
         
         val parentVals = vals.map { it + current.`val` }.toMutableList()
         parentVals.add(current.`val`)
@@ -23,5 +26,47 @@ class Solution {
         
         pathChild(current.left, parentVals, sum)
         pathChild(current.right, parentVals, sum)
+    }
+}
+
+
+// Using recursive
+class Solution {
+    fun pathSum(root: TreeNode?, sum: Int): Int {
+        root ?: return 0
+        
+        return pathSequenceSum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum)
+    }
+    
+    fun pathSequenceSum(node: TreeNode?, sum: Int): Int {
+        node ?: return 0
+        
+        var count = if (node.`val` == sum) 1 else 0
+        count += pathSequenceSum(node.left, sum - node.`val`) + pathSequenceSum(node.right, sum - node.`val`)
+        
+        return count
+    }
+}
+
+
+// Using hashmap
+// Time complexity: O(n)
+class Solution {
+    fun pathSum(root: TreeNode?, sum: Int): Int {
+        return recursive(root, 0, sum, hashMapOf(0 to 1))
+    }
+    
+    fun recursive(node: TreeNode?, accumulateSum: Int, sum: Int, map: HashMap<Int, Int?>): Int {
+        node ?: return 0
+        
+        val currSum = accumulateSum + node.`val`
+        var count =  map[currSum - sum] ?: 0
+        
+        map[currSum] = map[currSum]?.plus(1) ?: 1
+        count += recursive(node.left, currSum, sum, map)
+        count += recursive(node.right, currSum, sum, map)
+        map[currSum] = map[currSum]!! - 1
+        
+        return count
     }
 }
